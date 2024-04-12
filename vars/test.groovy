8744affa49037c58140ded9d7697377eb2022b48
Dawid Rabiega <d.rabiega@gmail.com>
2024-04-12 02:33:24 +0200

    test

--- vars/test.groovy
+++ vars/test.groovy
@@ -3,11 +3,12 @@ def call() {
     //    "logLevel": "INFO"
     //})
     //echo "logLevel: ${com.dawidcorp.infra.Config.logLevel}"
-    // if (com.dawidcorp.infra.Config.debug()) {
-    //     echo "Debugging is enabled"
-    // } else {
-    //     echo "Debugging is disabled"
-    // }
+
     echo "mainBranch: ${com.dawidcorp.infra.Config.mainBranch()}"
     echo "logLevel: ${com.dawidcorp.infra.Config.logLevel}"
+    if (com.dawidcorp.infra.Config.debug()) {
+        echo "Debugging is enabled"
+    } else {
+        echo "Debugging is disabled"
+    }
 }
